a479c8e072b5c90aa4b6809655cadc29c93797e8
sharimardinov <sharimardinov@icloud.com>
2024-11-09 22:02:43 +0300

    1

--- src/main/java/app/config/SecurityConfig.java
+++ src/main/java/app/config/SecurityConfig.java
@@ -46,7 +46,6 @@ public class SecurityConfig {
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout=true")
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                         .permitAll()
